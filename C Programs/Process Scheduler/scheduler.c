84cafdb5dbf26cef580d53d8ce603cf37a77273c
Derek Coleman <47635010+coleman7245@users.noreply.github.com>
2023-04-08 00:50:15 -0600

    Origin (#7)
    * Revamped the code to reduce bloat and improve performance--only for the FCFS, SJF, SJFPreempt, and Priority algorithms. Further updates will be made to the Lottery algorithm.
    
    * Added the AlgorithmOverhead struct to remove some clutter from the method signatures. Also, the lottery scheduler has been revamped. Finally, I removed some unnecessary methods.
    
    * Removed some unnecessary messages added by GitHub.
    
    * Fixed a host of bugs on the lottery scheduler method. Will return to clean things up and add comments.
    
    * Revamped the design so that the Process and SchedulerOverhead structs are in their own c files--along with their respective print methods.
    
    * Revamped the design to bring it closer to OOD principles.

--- C Programs/Process Scheduler/scheduler.c
+++ C Programs/Process Scheduler/scheduler.c
@@ -1,7 +1,6 @@
 //Author: coleman7245
 //Project: Process Scheduler Algorithms
 //File: Scheduler C File
-//Last Edit: Tuesday, April 4th, 2023
 
 #include "scheduler.h"
 
@@ -16,38 +15,17 @@ int main(int argc, char** argv)
 	
 	/*Variables*/
 	FILE *input_file = fopen(argv[1], "rb"); //Open the input file with the second argument.
-	SchedulerOverhead *overhead = malloc(sizeof(SchedulerOverhead)); //Allocate memory for the overhead.
-	overhead->current_pos = -1; //Set the current process to an invalid position.
-	overhead->size = findNewLines(input_file); //Set the size of the process list to the number of lines in the input file. 
-	overhead->time = 1; //Set the time to 1.
-	overhead->winning_ticket = 0; //Set the initial winning process to 0.
-	overhead->processes_left = 0; //Set the initial amount of processes left to run to 0.
-	overhead->option = 0; //Set the scheduler option to 0.
-	overhead->random_seed = atoi(argv[2]); //Set the random seed for lottery shceduling to the third argument.
-	overhead->highest_ticket = 0; //Set the highest ticket value of the processes to 0.
-	overhead->lowest_ticket = 999999; //Set the lowest ticket value of the processes to a massive number.
-	Process *data = malloc(sizeof(Process) * overhead->size); //Allocates memory for an array of processes.
+	SchedulerOverhead *overhead = SchedulerOverheadInit(-1, findNewLines(input_file), 1, 0, 0, 0, atoi(argv[2]), 0, 999999); //Allocate and initialize the scheduler overhead.
+	Process **data = malloc(overhead->size * sizeof(Process**));
 	
-	for (int pos = 0; pos < overhead->size; pos++) //From the beginning to the end of the file...
+	if (data == NULL)
 	{
-		//Initialize all values of the processes in the list to 0.
-		data[pos].pid = 0;
-		data[pos].arrival_time = 0;
-		data[pos].burst_duration = 0;
-		data[pos].priority = 0;
-		data[pos].waiting_time = 0;
-		data[pos].turnaround_time = 0;
-		data[pos].duration_left = 0;
-		data[pos].last_runtime = 0;
-		data[pos].num_of_tickets = 0;
-		data[pos].lower_bound_tickets = malloc(sizeof(int) * overhead->size); //Allocate memory for the ticket list.
-		data[pos].upper_bound_tickets = malloc(sizeof(int) * overhead->size); //Alocate memory for the ticket list.
-		
-		for (int t_pos = 0; t_pos < overhead->size; t_pos++)
-		{
-			data[pos].lower_bound_tickets[t_pos] = 0;
-			data[pos].upper_bound_tickets[t_pos] = 0;
-		}
+		return 0;
+	}
+	
+	for (int pos = 0; pos < overhead->size; pos++)
+	{
+		data[pos] = ProcessInit(overhead->size);
 	}
 	
 	if (!readFile(input_file, overhead, data)) //If the input file read failed, then...
@@ -71,31 +49,29 @@ int main(int argc, char** argv)
 	
 	for (int pos = 0; pos < overhead->size; pos++) //From the beginning to the end of the file...
 	{
-		free(data[pos].lower_bound_tickets); //Free the memory allocated for the list of lower ticket ranges.
-		data[pos].lower_bound_tickets = NULL;
-		free(data[pos].upper_bound_tickets); //Free the memory allocated for the list of upper ticket ranges.
-		data[pos].upper_bound_tickets = NULL;
+		ProcessDecon(data[pos], overhead->size);
+		data[pos] = NULL;
 	}
 
 	free(data); //Free the memory allocated for the list of processes.
 	data = NULL; //Set the pointer to null.
-	free(overhead); //Free the memory allocated for the overhead.
+	SchedulerOverheadDecon(overhead); //Free the memory allocated for the overhead.
 	overhead = NULL; //Set the pointer to null.
 	
 	return 1; //If everything performed accordingly, return true.
 }
 
 /*Auxiliary functions*/
-int readFile(FILE *input_file, SchedulerOverhead *overhead, Process *processes)
+int readFile(FILE *input_file, SchedulerOverhead *overhead, Process **processes)
 {
 	/*Variable*/
 	int r_pos = 0; //Position within the process array.
 	
 	while (!feof(input_file)) //While the pointer is not at the end of the file...
 	{	
-		if (!fscanf(input_file, "%d,%d,%d,%d,%d - %d\n", &processes[r_pos].pid, &processes[r_pos].arrival_time, 
-			&processes[r_pos].burst_duration, &processes[r_pos].priority, &processes[r_pos].lower_bound_tickets[0], 
-			&processes[r_pos].upper_bound_tickets[0])) //Read a string according to the specified format, then extract the integers and store them in their proper place. If the read fails...
+		if (!fscanf(input_file, "%d,%d,%d,%d,%d - %d\n", &processes[r_pos]->pid, &processes[r_pos]->arrival_time, 
+			&processes[r_pos]->burst_duration, &processes[r_pos]->priority, &processes[r_pos]->lower_bound_tickets[0], 
+			&processes[r_pos]->upper_bound_tickets[0])) //Read a string according to the specified format, then extract the integers and store them in their proper place. If the read fails...
 			{
 				if (feof(input_file)) //If the end of file has been reached...
 				{
@@ -111,19 +87,19 @@ int readFile(FILE *input_file, SchedulerOverhead *overhead, Process *processes)
 		//printf("from %s: processes[%d].lower_bound_tickets[%d] = %d\n", "readFile", r_pos, r_pos, processes[r_pos].lower_bound_tickets[r_pos]);
 		//printf("from %s: processes[%d].upper_bound_tickets[%d] = %d\n", "readFile", r_pos, r_pos, processes[r_pos].upper_bound_tickets[r_pos]);
 			
-		if (processes[r_pos].lower_bound_tickets[0] < overhead->lowest_ticket)
+		if (processes[r_pos]->lower_bound_tickets[0] < overhead->lowest_ticket)
 		{
-			overhead->lowest_ticket = processes[r_pos].lower_bound_tickets[0];
+			overhead->lowest_ticket = processes[r_pos]->lower_bound_tickets[0];
 		}
 		
-		if (processes[r_pos].upper_bound_tickets[0] > overhead->highest_ticket)
+		if (processes[r_pos]->upper_bound_tickets[0] > overhead->highest_ticket)
 		{
-			overhead->highest_ticket = processes[r_pos].upper_bound_tickets[0];
+			overhead->highest_ticket = processes[r_pos]->upper_bound_tickets[0];
 		}
 		
-		processes[r_pos].waiting_time = -processes[r_pos].arrival_time;
-		processes[r_pos].num_of_tickets = 1; //Initialize the number tickets the process holds to 1.
-		processes[r_pos].duration_left = processes[r_pos].burst_duration; //Set the burst duration left to the maximum burst duration.
+		processes[r_pos]->waiting_time = -processes[r_pos]->arrival_time;
+		processes[r_pos]->num_of_tickets = 1; //Initialize the number tickets the process holds to 1.
+		processes[r_pos]->duration_left = processes[r_pos]->burst_duration; //Set the burst duration left to the maximum burst duration.
 		
 		r_pos++; //Increment the position in the array of new processes.
 	}
@@ -251,13 +227,13 @@ int findNewLines(FILE *open_file)
 	}
 }
 
-int displayMenu(Process *processes, SchedulerOverhead *overhead)
+int displayMenu(Process **processes, SchedulerOverhead *overhead)
 {
 	/*Variables*/
 	Process *current_list = malloc(sizeof(Process) * overhead->size); //List of processes.
 	int option = 0; //Storage for menu options.;
 		
-	if (current_list == (Process *)NULL) //If the SJFnonPre method failed, then...
+	if (current_list == NULL) //If the SJFnonPre method failed, then...
 	{
 		return 0; //Return false;
 	}
@@ -266,44 +242,26 @@ int displayMenu(Process *processes, SchedulerOverhead *overhead)
 	{
 		current_list[pos].lower_bound_tickets = malloc(sizeof(int) * overhead->size);
 		
-		if (current_list[pos].lower_bound_tickets == (int *)NULL) //If the SJFnonPre method failed, then...
+		if (current_list[pos].lower_bound_tickets == NULL) //If the SJFnonPre method failed, then...
 		{
 			return 0; //Return false;
 		}
 		
 		current_list[pos].upper_bound_tickets = malloc(sizeof(int) * overhead->size);
 			
-		if (current_list[pos].upper_bound_tickets == (int *)NULL) //If the SJFnonPre method failed, then...
+		if (current_list[pos].upper_bound_tickets == NULL) //If the SJFnonPre method failed, then...
 		{
 			return 0; //Return false;
 		}
+		
+		initializeProcess(&current_list[pos], overhead->size);
 	}
 	
 	do
-	{
+	{	
 		for (int pos = 0; pos < overhead->size; pos++)
 		{
-			current_list[pos].pid = processes[pos].pid;
-			current_list[pos].arrival_time = processes[pos].arrival_time;
-			current_list[pos].burst_duration = processes[pos].burst_duration;
-			current_list[pos].priority = processes[pos].priority;
-			current_list[pos].waiting_time = processes[pos].waiting_time;
-			current_list[pos].turnaround_time = processes[pos].turnaround_time;
-			current_list[pos].duration_left = processes[pos].duration_left;
-			current_list[pos].last_runtime = processes[pos].last_runtime;
-			current_list[pos].num_of_tickets = processes[pos].num_of_tickets;
-			printf("from %s: processes[%d].num_of_tickets = %d\n", "displayMenu", pos, processes[pos].num_of_tickets);
-			printf("from %s: current_list[%d].num_of_tickets = %d\n", "displayMenu", pos, current_list[pos].num_of_tickets);
-			
-			for (int t_pos = 0; t_pos < overhead->size; t_pos++)
-			{
-				//printf("from %s: processes[%d].lower_bound_tickets[%d] = %d\n", "displayMenu", pos, t_pos, processes[pos].lower_bound_tickets[t_pos]);
-				current_list[pos].lower_bound_tickets[t_pos] = processes[pos].lower_bound_tickets[t_pos]; //Reset the lower bound ticket range to the original value for each process.
-				//printf("from %s: current_list[%d].lower_bound_tickets[%d] = %d\n", "displayMenu", pos, t_pos, current_list[pos].lower_bound_tickets[t_pos]);
-				//printf("from %s: processes[%d].upper_bound_tickets[%d] = %d\n", "displayMenu", pos, t_pos, processes[pos].upper_bound_tickets[t_pos]);
-				current_list[pos].upper_bound_tickets[t_pos] = processes[pos].upper_bound_tickets[t_pos]; //Reset the upper bound ticket range to the original value for each process.
-				//printf("from %s: current_list[%d].upper_bound_tickets[%d] = %d\n", "displayMenu", pos, t_pos, current_list[pos].upper_bound_tickets[t_pos]);
-			}
+			copyProcess(processes[pos], &current_list[pos], overhead->size);
 		}
 		
 		//Prints out the menu on the command console.
@@ -367,8 +325,6 @@ int findProcess(Process *processes, SchedulerOverhead *overhead)
 {
 	int new_pos = -1;
 	
-	printSchedulerOverhead("findProcess", overhead);
-	
 	if (overhead->option == 1)
 	{
 		for (int pos = 0; pos <= overhead->size; pos++) //From the beginning to the end of the list of processes...
@@ -424,8 +380,6 @@ int findProcess(Process *processes, SchedulerOverhead *overhead)
 	{		
 		for (int pos = 0; pos < overhead->size; pos++) //From the beginning to the end of the list of processes...
 		{
-			printf("from findProcess: pos = %d\n", pos);
-			
 			if (processes[pos].arrival_time <= overhead->time &&
 				processes[pos].duration_left > 0) //Check if the process arrived on time and has yet to run.
 			{
@@ -442,17 +396,10 @@ int findProcess(Process *processes, SchedulerOverhead *overhead)
 		}
 	}
 	
-	//printf("From findProcess: processes[%d].waiting_time = %d\n", new_pos, processes[new_pos].waiting_time);
-	//printf("From findProcess: time = %d\n", overhead->time);
-	//printf("From findProcess: processes[%d].last_runtime = %d\n", new_pos, processes[new_pos].last_runtime);
-	
 	if (new_pos != -1)
 	{
 		processes[new_pos].waiting_time += overhead->time - processes[new_pos].last_runtime;
 	}
-	
-	printf("From findProcess: new_pos = %d\n", new_pos);
-	//printf("From findProcess: processes[%d].waiting_time = %d\n", new_pos, processes[new_pos].waiting_time);
 	
 	return new_pos;
 }
@@ -481,10 +428,6 @@ void FCFS(Process *processes, SchedulerOverhead *overhead)
 	
 	while (overhead->processes_left > 0) //While the number of processes left is greater than 0...
 	{	
-		printf("From FCFS: Current Process ID = %d\n", processes[overhead->current_pos].pid);
-		printf("From FCFS: processes[%d].duration_left = %d\n", overhead->current_pos, processes[overhead->current_pos].duration_left);
-		printf("From FCFS: time = %d\n", overhead->time);
-		
 		if (processes[overhead->current_pos].duration_left <= 0) //Else, if the process has finished with the CPU, then...
 		{	
 			overhead->current_pos = switchProcesses(processes, overhead);
@@ -508,10 +451,6 @@ void SJF(Process *processes, SchedulerOverhead *overhead, int preempt)
 	
 	while (overhead->processes_left > 0) //While the number of processes left is greater than 0...
 	{
-		printf("From SJF: Current Process ID = %d\n", processes[overhead->current_pos].pid);
-		printf("From SJF: processes[%d].duration_left = %d\n", overhead->current_pos, processes[overhead->current_pos].duration_left);
-		printf("From SJF: time = %d\n", overhead->time);
-		
 		if (processes[overhead->current_pos].duration_left <= 0) //Else, if the process has finished with the CPU, then...
 		{	
 			overhead->current_pos = switchProcesses(processes, overhead);
@@ -563,10 +502,6 @@ void PriorityScheduling(Process *processes, SchedulerOverhead *overhead)
 	
 	while (overhead->processes_left > 0) //While the number of processes left is greater than 0...
 	{	
-		printf("From PriorityScheduling: Current Process ID = %d\n", processes[overhead->current_pos].pid);
-		printf("From PriorityScheduling: processes[%d].duration_left = %d\n", overhead->current_pos, processes[overhead->current_pos].duration_left);
-		printf("From PriorityScheduling: time = %d\n", overhead->time);
-		
 		if (processes[overhead->current_pos].duration_left <= 0) //Else, if the process has finished with the CPU, then...
 		{	
 			overhead->current_pos = switchProcesses(processes, overhead);
@@ -582,20 +517,15 @@ void PriorityScheduling(Process *processes, SchedulerOverhead *overhead)
 
 int findWinningTicket(Process *process, SchedulerOverhead *overhead)
 {	
-	printTickets("findWinningTicket", process, process->num_of_tickets);
-	printf("from findWinningTicket: overhead->winning_ticket = %d\n", overhead->winning_ticket);
-		
 	for (int t_pos = 0; t_pos < process->num_of_tickets; t_pos++)
 	{
 		if (overhead->winning_ticket >= process->lower_bound_tickets[t_pos] && 
 			overhead->winning_ticket <= process->upper_bound_tickets[t_pos])
 			{
-				printf("from findWinningTicket: Process %d won!\n", process->pid);
 				return 1;
 			}
 	}
 	
-	printf("from findWinningTicket: Process %d lost!\n", process->pid);
 	return 0;
 }
 
--- C Programs/Process Scheduler/scheduleroverhead.c
+++ C Programs/Process Scheduler/scheduleroverhead.c
@@ -1,13 +1,45 @@
 //Author: coleman7245
 //Project: Process Scheduler Algorithms
 //File: Scheduler Overhead C File
-//Last Edit: Tuesday, April 4th, 2023
 
 #include "scheduleroverhead.h"
-#include <stdio.h>
+
+SchedulerOverhead* SchedulerOverheadInit(int current_pos, int size, int time, int winning_ticket, int processes_left, int option, int random_seed, int highest_ticket, int lowest_ticket)
+{
+	SchedulerOverhead *overhead = malloc(sizeof(SchedulerOverhead)); //Allocate memory for the scheduler overhead according to the given size.
+	
+	if (overhead == NULL)
+	{
+		return NULL;
+	}
+	
+	initializeSchedulerOverhead(overhead, current_pos, size, time, winning_ticket, processes_left, option, random_seed, highest_ticket, lowest_ticket); //Set all the variables of the scheduler overhead.
+	
+	return overhead; //Return the scheduler overhead pointer.
+}
+
+void SchedulerOverheadDecon(SchedulerOverhead *overhead)
+{
+	free(overhead);
+}
+
+void initializeSchedulerOverhead(SchedulerOverhead *overhead, int current_pos, int size, int time, int winning_ticket, int processes_left, int option, int random_seed, int highest_ticket, int lowest_ticket)
+{
+	/*Initialize all struct variables to 0.*/
+	overhead->current_pos = current_pos;
+	overhead->size = size;
+	overhead->time = time;
+	overhead->winning_ticket = winning_ticket;
+	overhead->processes_left = processes_left;
+	overhead->option = option;
+	overhead->random_seed = random_seed;
+	overhead->highest_ticket = highest_ticket;
+	overhead->lowest_ticket = lowest_ticket;
+}
 
 void printSchedulerOverhead(const char *method_name, SchedulerOverhead *overhead)
 {
+	/*Print all variables of the struct */
 	printf("from %s: overhead->current_pos = %d\n", method_name, overhead->current_pos);
 	printf("from %s: overhead->size = %d\n", method_name, overhead->size);
 	printf("from %s: overhead->time = %d\n", method_name, overhead->time);
